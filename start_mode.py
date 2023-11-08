f1028bbbfc4dbe93c63a0ad4ab851b1115a8a0d9
yallmoong <victoria0404@naver.com>
2023-11-08 23:27:26 +0900

    handle_events() 함수 완성

--- start_mode.py
+++ start_mode.py
@@ -1,8 +1,17 @@
 from pico2d import *
 
+import game_framework
+
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.change_mode(stage_mode)
 
 
 def init():
