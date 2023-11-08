9737d63fb9600da931fa988f338a31cde57d44d2
yallmoong <victoria0404@naver.com>
2023-11-09 02:28:44 +0900

    스테이지 모드 선택 효과 완성

--- stage.py
+++ stage.py
@@ -1,18 +1,24 @@
 from pico2d import *
 
-image_names = ['basic', 'wind', 'sheep', 'poop', 'TomYumGoong']
+image_names = ('basic', 'wind', 'sheep', 'poop', 'TomYumGoong')
 
 class Stage:
     def __init__(self):
         self.image = [load_image('./resource/stage_' + image_names[i] + '.png') for i in range(5)]
+        self.x = (248, 671, 1170, 466, 1031)
+        self.y = (320, 348, 292, 618, 618)
+        self.w = (340, 346, 296, 298, 314)
+        self.h = (322, 218, 332, 236, 294)
+        self.select = -1
 
     def update(self):
         pass
 
     def draw(self):
-        self.image[0].draw(248, 800 - 320)
-        self.image[1].draw(671, 800 - 348)
-        self.image[2].draw(1170, 800 - 292)
-        self.image[3].draw(466, 800 - 618)
-        self.image[4].draw(1031, 800 - 618)
+        for i in range(5):
+            if self.select == i:
+                self.image[i].draw(self.x[i], 800 - self.y[i], self.w[i] * 1.1, self.h[i] * 1.1)
+            else:
+                self.image[i].draw(self.x[i], 800 - self.y[i], self.w[i], self.h[i])
+            # draw_rectangle(self.x[i] - self.w[i] / 2, 800 - (self.y[i] - self.h[i] / 2), self.x[i] + self.w[i] / 2, 800 - (self.y[i] + self.h[i] / 2))
 
--- stage_mode.py
+++ stage_mode.py
@@ -8,7 +8,10 @@ from stage_background import StageBackground
 
 
 def handle_select(x, y):
-    pass
+    for i in range(5):
+        if x <= stage.x[i] + stage.w[i] / 2 and x >= stage.x[i] - stage.w[i] / 2 and y <= stage.y[i] + stage.h[i] / 2 and y >= stage.y[i] - stage.h[i] / 2:
+            stage.select = i
+            return
 
 
 def handle_events():
@@ -19,8 +22,11 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_world.clear()
             game_framework.change_mode(start_mode)
-        elif event.type == SDL_MOUSEBUTTONDOWN and event.key == SDL_BUTTON_LEFT:
+        elif event.type == SDL_MOUSEMOTION:
             handle_select(event.x, event.y)
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.key == SDL_BUTTON_LEFT:
+            if stage.select >= 0:
+                pass
 
 
 def init():
