fa6436429eb1d54f9fe5f0c1c1c46f04a05d2afa
yallmoong <victoria0404@naver.com>
2023-11-14 18:50:37 +0900

    스테이지 인트로 애니메이션 완성

--- stage_intro.py
+++ stage_intro.py
@@ -2,19 +2,21 @@ from pico2d import *
 
 import game_framework
 import stage_aim_mode
+import stage_list_mode
+
 
 class StageIntro:
     def __init__(self):
         self.image = load_image('./resource/stage_title.png')
         self.x = -475.0
         self.intro_start_time = get_time()
-        self.font = load_font('ENCR10B.TTF', 16)
-
+        self.font = load_font('BMEULJIRO.otf', 120)
 
     def update(self):
-        self.x += 8.3 * (get_time() - self.intro_start_time - 1) ** 2
+        self.x += 9.5 * (get_time() - self.intro_start_time - 1) ** 2
         if get_time() - self.intro_start_time >= 2.0:
             game_framework.change_mode(stage_aim_mode)
 
     def draw(self):
         self.image.draw(self.x, 400)
+        self.font.draw(self.x - 160, 400, f'stage{stage_list_mode.stage_list.select + 1}', (0, 0, 0))
