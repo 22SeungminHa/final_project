bd099d56bc58b8a0dd3c5bc7f8a8345967651d27
yallmoong <victoria0404@naver.com>
2023-11-23 04:56:03 +0900

    좌표 계산 완성

--- stage_aim_mode.py
+++ stage_aim_mode.py
@@ -1,4 +1,4 @@
-from pico2d import*
+from pico2d import *
 
 import game_framework
 import game_world
@@ -9,6 +9,10 @@ import stage_title_mode
 from background import Background
 from object_bow import Bow
 from object_target import Target
+
+
+def calculate_win(x, y):
+    return (2 * x - 1422) / 2, -(2 * y - 800) / 2
 
 
 def init():
@@ -27,13 +31,15 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.pop_mode()
             game_framework.push_mode(stage_list_mode)
+        elif event.type == SDL_MOUSEMOTION:
+            bow.mx, bow.my = calculate_win(event.x, event.y)
+            # print(bow.mx, bow.my)
         elif event.button == SDL_BUTTON_LEFT:
             if event.type == SDL_MOUSEBUTTONDOWN:
                 bow.animation = 'zoom in'
             else:
                 game_framework.pop_mode()
                 game_framework.push_mode(stage_launch_mode)
-
 
 
 def finish():
@@ -56,6 +62,7 @@ def pause():
 
     bow = Bow()
     game_world.add_object(bow, 1)
+
 
 def resume():
     pass
