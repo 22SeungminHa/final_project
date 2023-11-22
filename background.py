b243f3a3744d4c146a996afb04a2b71605816950
yallmoong <victoria0404@naver.com>
2023-11-22 23:34:47 +0900

    background.py 완성

--- background.py
+++ background.py
@@ -2,8 +2,16 @@ from pico2d import *
 
 
 class Background:
+
     def __init__(self):
-        self.image = load_image('./resource/palace7.png')
+        self.image = None
+        load_image(self)
+
+    def load_image(self):
+        if self.image == None:
+            self.image = {}
+            for i in range(10):
+                self.image[i] = load_image("./resource/palace" + " (%d)" % i + ".png")
 
     def update(self):
         pass
