766375d338181cadc9f9f4329520d52b225f8a9f
yallmoong <victoria0404@naver.com>
2023-11-22 23:52:38 +0900

    배경 코드 수정

--- background.py
+++ background.py
@@ -2,20 +2,28 @@ from pico2d import *
 
 
 class Background:
-
     def __init__(self):
-        self.image = None
+        self.palace = []
+        self.mode = []
+        self.tyg = []
+        self.index, self.type = 0, 'm'
         load_image(self)
 
     def load_image(self):
-        if self.image == None:
-            self.image = {}
-            for i in range(10):
-                self.image[i] = load_image("./resource/palace" + " (%d)" % i + ".png")
+        for i in range(10):
+            self.palace[i] = load_image("./resource/palace" + "%d" % (i + 1) + ".png")
+        for i in range(3):
+            self.tyg[i] = load_image("./resource/Tom_Yum_Goong" + "%d" % (i + 1) + ".png")
+        self.mode.append(load_image("./resource/start_background"))
+        self.mode.append(load_image("./resource/stage_background"))
 
     def update(self):
         pass
 
-    def draw(self):
-        self.image.draw(711, 400, 1422, 800)
-
+    def draw(self, image_type, index):
+        if image_type == 'm':
+            self.mode[index].draw(711, 400, 1422, 800)
+        elif image_type == 'p':
+            self.palace[index].draw(711, 400, 1422, 800)
+        elif image_type == 't':
+            self.tyg[index].draw(711, 400, 1422, 800)
