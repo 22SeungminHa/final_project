f39c962a6f58be67a48ac3386ab31324d73c3ba6
yallmoong <victoria0404@naver.com>
2023-12-01 05:18:44 +0900

    런처모드 완성

--- object_target.py
+++ object_target.py
@@ -2,12 +2,16 @@ from pico2d import *
 
 
 class Target:
-    def __init__(self):
+    def __init__(self, mode):
         self.image = load_image('./resource/target.png')
+        self.mode = mode
+        self.size = 1.0
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(711, 400, 200, 200)
-
+        if self.mode == 'aim':
+            self.image.draw(711, 400, 200, 200)
+        elif self.mode == 'launch':
+            self.image.draw(711, 400, 300, 300)
--- stage_launch_mode.py
+++ stage_launch_mode.py
@@ -4,28 +4,40 @@ from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
 import game_framework
 import game_world
 import stage
+import stage_aim_mode
 from background import Background
 from object_arrow import Arrow
 from object_target import Target
 
 
+score = 0
+aim_x, aim_y = 0, 0
+
+def cal_distance(x1, y1, x2, y2):
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+def cal_score(x, y):
+    global score
+    score = 10 - cal_distance(x, y, 0, 0) // 16
+
+
 def init():
     global arrow
     global target
-    global x, y
 
-    x = stage_aim
+    cal_score(aim_x, aim_y)
 
-    arrow = Arrow()
-    target = Target()
+    arrow = Arrow(aim_x, aim_y)
+    target = Target('launch')
 
     game_world.add_object(arrow, 2)
     game_world.add_object(target, 1)
 
 
 def finish():
-    global image
-    del image
+    game_world.remove_object(target)
+    game_world.remove_object(arrow)
 
 
 def handle_events():
@@ -35,15 +47,21 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            game_framework.change_mode(play_mode)
 
 
 def update():
-    pass
+    game_world.update()
 
 
 def draw():
     clear_canvas()
-    image.draw(400, 300)
+    game_world.render()
     update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
