c6de9729e3666edb121dc44cec76aec9bbc25122
yallmoong <victoria0404@naver.com>
2023-12-06 06:35:39 +0900

    로고모드로 시작

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pico2d import open_canvas, delay, close_canvas
 
 import clear_state
 import game_framework
-import background_mode as start_mode
+import logo_mode as start_mode
 
 clear_state.read_2d_list_from_file('stage.json')
 open_canvas(1422, 800)
